7cb6827cbcbc9b91178fe7d2adafbd0c3376b36d
Bajrang Ingle <bj2310@example.com>
2020-06-07 10:19:59 +0530

    Adding Home Page.

--- src/test/java/com/orangehrm/pageObjects/HomePage.java
+++ src/test/java/com/orangehrm/pageObjects/HomePage.java
@@ -1,5 +1,14 @@
 package com.orangehrm.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+	WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+	
 
 }
